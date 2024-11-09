de424837207e01e977736fb627a3c01431b28dca
Hugo Cruz <132757752+hugocruz13@users.noreply.github.com>
2024-11-09 17:01:43 +0000

    documentation

--- src/Employee.cs
+++ src/Employee.cs
@@ -3,7 +3,7 @@
 *		Copyright (c) 2024 All Rights Reserved
 *	</copyright>
 * 	<author>hugoc</author>
-*   <date>11/2/2024</date>
+*   <date>11/2/2024 3:20:10 PM</date>
 *	<description>This file is focused on employees.</description>
 **/
 using System;
@@ -11,7 +11,7 @@ using System;
 namespace src
 {
     /// <summary>
-    /// Purpose: Class <c>Employee</c> allows to create an employee object with basic 
+    /// Class <c>Employee</c> allows to create an employee object with basic 
     /// information such as their Id, name, role, and hourly role.
     /// </summary>
     /// <remarks>
@@ -21,15 +21,28 @@ namespace src
     /// Example of use
     /// <code>
     /// Employee emp = new Employee();
-    /// emp.Role= "Engineering";
-    /// emp.HourlyRate = "12.3";
+    /// emp.Name = "Hugo Cruz";
+    /// emp.Role = "Engineering";
+    /// emp.HourlyRate = 12.3;
+    /// </code>
+    /// <code> 
+    /// Employee emp = new Employee("Hugo Cruz", "Engineering", 12.3);
     /// </code>
     /// </example>
-    class Employee : Person
+    public class Employee : Person
     {
         #region Attributes
+        /// <summary>
+        /// The position of employee.
+        /// </summary>
         string role;
+        /// <summary>
+        /// The hourly rate of the employee.
+        /// </summary>
         double hourlyRate;
+        /// <summary>
+        /// Static counter to assign unique IDs to employees.
+        /// </summary>
         static int employeeIdCounter = 200;
         #endregion
 
@@ -38,8 +51,9 @@ namespace src
         #region Properties
 
         /// <summary>
-        /// Employee Role.
+        /// Obtain or define the position of the employee.
         /// </summary>
+        /// <value>The position of employee.</value>
         /// <permission>
         /// Public Access
         /// </permission>
@@ -50,8 +64,9 @@ namespace src
         }
 
         /// <summary>
-        /// Employee Hourly Rate
+        /// Gets or sets the employee’s hourly rate
         /// </summary>
+        /// <value>The hourly rate of the employee.</value>
         /// <permission>
         /// Public Access
         /// </permission>
@@ -65,29 +80,33 @@ namespace src
         #region Constructors
 
         /// <summary>
-        /// The default Constructor.
+        /// Default constructor for the employee. Automatically assigns an ID 
+        /// and initializes the other attributes with default values.
         /// </summary>
+        /// <remarks>
+        /// The ID is automatically generated with the static counter.
+        /// The name, position and hourly rate are initialized as string empty or zero.
+        /// </remarks>
         /// <permission>
         /// Public Access
         /// </permission>
         public Employee()
         {
             Id = employeeIdCounter++;
-            Name = null;
-            Role = null;
+            Name = string.Empty;
+            Role = string.Empty;
             HourlyRate = 0;
         }
 
         /// <summary>
-        /// Simple constructor for employees.
+        /// Simple constructor to initialize the employee with specific values.
         /// </summary>
         /// <param name="name">Employee Name</param>
         /// <param name="role">Employee Role</param>
         /// <param name="hourlyrate">Employee Hourly Rate</param>
         /// <remarks>
-        /// Initializes an object 'Employee' with the specified values 
-        /// for name, role, and hourly rate. The ID is assigned automatically
-        /// using the static counter.
+        /// Initializes the employee with a given name, job title and hourly rate.
+        /// The ID is assigned automatically using the static counter.
         /// </remarks>
         public Employee(string name, string role, double hourlyrate)
         {
@@ -96,7 +115,6 @@ namespace src
             Role = role;
             HourlyRate = hourlyrate;
         }
-
         #endregion
 
         #region Overrides
@@ -107,7 +125,7 @@ namespace src
 
         #region Destructor
         /// <summary>
-        /// The destructor.
+        /// The destroyer of the Employee class.
         /// </summary>
         ~Employee()
         {
