76f15b3fc7ca11ab01a6a3fb9297d067568b7bf3
Hugo Cruz <132757752+hugocruz13@users.noreply.github.com>
2024-11-09 17:01:34 +0000

    documentation

--- src/Client.cs
+++ src/Client.cs
@@ -1,37 +1,55 @@
 ﻿/*
-*	<copyright file="Person.cs" company="IPCA">
+*	<copyright file="Client.cs" company="IPCA">
 *		Copyright (c) 2024 All Rights Reserved
 *	</copyright>
 * 	<author>hugoc</author>
 *   <date>11/3/2024 7:20:50 PM</date>
-*	<description></description>
+*	<description>This file is focused on creating client objects (Client).</description>
 */
 using System;
 
 namespace src
 {
     /// <summary>
-    /// Purpose:
-    /// Created by: hugoc
-    /// Created on: 11/3/2024 7:20:50 PM
+    /// The <c>Client</c> class allows you to create a client object
+    /// with basic information such as ID, name and contact information.
     /// </summary>
-    /// <remarks></remarks>
-    /// <example></example>
-
-    class Client : Person
+    /// <remarks>
+    /// This class inherits from <see cref="Person"/> and provides methods to access and modify 
+    /// the client attributes, and assign an ID automatically using a static counter.
+    /// </remarks>
+    /// <example>
+    /// Example of use
+    /// <code>
+    /// Client c = new Client("Maria Rodrigues", "9323423553")
+    /// </code>
+    /// </example>
+    public class Client : Person
     {
         #region Attributes
+        /// <summary>
+        /// Customer contact information.
+        /// </summary>
         string contactInfo;
+        /// <summary>
+        /// Static counter to assign unique IDs to clients.
+        /// </summary>
         static int clientIdCounter = 500;
         #endregion
 
         #region Methods
 
         #region Properties
-
+        /// <summary>
+        /// Gets or sets the customer’s contact information.
+        /// </summary>
+        /// <value>The customer’s contact information</value>
+        /// <permission>
+        /// Public Access.
+        /// </permission>
         public string ContactInfo
         {
-            set { contactInfo = value; }
+            set  { contactInfo = value; }
             get { return contactInfo; }
         }
         #endregion
@@ -39,8 +57,16 @@ namespace src
         #region Constructors
 
         /// <summary>
-        /// The default Constructor.
+        /// Default builder for the client.
+        /// Automatically assigns an ID and initializes the other attributes with default values.
         /// </summary>
+        /// <remarks>
+        /// The ID is automatically generated with the static counter.
+        /// The name and contact information are initialized as empty.
+        /// </remarks>
+        /// <permission>
+        /// Public Access.
+        /// </permission>
         public Client()
         {
             Id = clientIdCounter++;
@@ -49,6 +75,18 @@ namespace src
 
         }
 
+        /// <summary>
+        /// Simple constructor to initialize or client with specific values.
+        /// </summary>
+        /// <param name="name">Name of customer.</param>
+        /// <param name="contact">Customer contact information.</param>
+        /// <remarks>
+        /// Initializes the client with a given name and contact information.
+        /// The ID is assigned automatically using the static counter.
+        /// </remarks>
+        /// <permission>
+        /// Public Access.
+        /// </permission>
         public Client(string name, string contact)
         {
             Id = clientIdCounter++;
@@ -67,7 +105,7 @@ namespace src
 
         #region Destructor
         /// <summary>
-        /// The destructor.
+        /// The destroyer of the Client class.
         /// </summary>
         ~Client()
         {
